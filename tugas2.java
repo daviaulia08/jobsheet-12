de334f85c95f303873bb25c59630761bdcc51d65
Your Name <you@example.com>
2024-12-16 04:57:27 +0700

    tugas ke-2

---
+++ tugas2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class tugas2 {
+    // Fungsi rekursif 
+    static int jumlahRekursif(int n) {
+        if (n == 1) {
+            System.out.print("1"); 
+            return 1;
+        } else {
+            System.out.print(n + " + "); 
+            return n + jumlahRekursif(n - 1); 
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        System.out.print("Perhitungan: ");
+        int hasil = jumlahRekursif(n);
+        System.out.println(" = " + hasil); 
+    }
+}
